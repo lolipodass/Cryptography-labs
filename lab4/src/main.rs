8952999a7fcaea9590706dfe9a9a9a93c22223de
kovalev Andrey <andremae10@gmail.com>
2024-05-09 12:40:20 +0300

    add changable parametrs into lab4

--- lab4/src/main.rs
+++ lab4/src/main.rs
@@ -5,13 +5,22 @@ use crate::iterative_code::IterativeCode;
 mod iterative_code;
 
 fn main() {
-    let mut code = IterativeCode::new_empty(6, 5);
+    let k1 = 6;
+    let k2 = 5;
+    let inf_size = k1 * k2;
+    let mut code = IterativeCode::new_empty(k1, k2);
 
-    let data = "010011011101101101011101011011";
-    let bool_data: Vec<bool> = data
-        .chars()
-        .map(|x| x == '1')
-        .collect();
+    //if binary string
+    // let data = "010011011101101101011101011011";
+    // let bool_data: Vec<bool> = data
+    //     .chars()
+    //     .map(|x| x == '1')
+    //     .collect();
+
+    let bool_data = random_binary_vec(inf_size);
+
+    print!("original word \t{}\n", vec_bool_to_string(&bool_data));
+
     let mut encode = code.encode(bool_data);
 
     print!("encoded word \t{}", vec_bool_to_string(&encode));
@@ -42,4 +51,9 @@ fn vec_bool_to_string(vec: &Vec<bool>) -> String {
         .into_iter()
         .map(|b| if b { '1' } else { '0' })
         .collect()
+}
+
+fn random_binary_vec(length: usize) -> Vec<bool> {
+    let mut rng = rand::thread_rng();
+    (0..length).map(|_| rng.gen_bool(0.5)).collect()
 }
