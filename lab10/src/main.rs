fd0eb7ac0810e1886eee48aa52a53b38a88c1fa5
kovalev Andrey <andremae10@gmail.com>
2024-05-15 12:19:58 +0300

    add all labs

--- lab10/src/main.rs
+++ lab10/src/main.rs
@@ -1,3 +1,143 @@
+use std::{ collections::HashMap, time::Instant };
+
+use bigdecimal::BigDecimal;
+
+fn compute_freq_map(message: String) -> Vec<(char, BigDecimal)> {
+    let mut frequency_map = HashMap::new();
+    let length: usize = message.chars().count();
+    let prec_size: u64 = u64::try_from(length.clone()).unwrap();
+
+    for c in message.chars() {
+        let count = frequency_map.entry(c).or_insert(BigDecimal::from(0));
+        *count += BigDecimal::from(1);
+    }
+
+    let mut res: Vec<_> = frequency_map
+        .iter()
+        .map(|(char, count)| (*char, count / BigDecimal::from(length as i64).with_prec(prec_size)))
+        .collect();
+
+    res.sort_by(|(_, a), (_, b)| a.cmp(b));
+    res
+}
+
+fn start_interval(alphabet: Vec<(char, BigDecimal)>) -> Vec<(char, BigDecimal, BigDecimal)> {
+    let mut interval: Vec<(char, BigDecimal, BigDecimal)> = Vec::new();
+    let mut min: BigDecimal = BigDecimal::from(0);
+
+    for item in &alphabet {
+        if item == &alphabet[0] {
+            interval.push((alphabet[0].0.clone(), BigDecimal::from(0), alphabet[0].1.clone()));
+            min = alphabet[0].1.clone();
+        } else {
+            interval.push((item.0.clone(), min.clone(), min.clone() + item.1.clone()));
+            min = min.clone() + item.1.clone();
+        }
+    }
+
+    interval
+}
+
+fn calculate_interval(
+    start_interval: Vec<(char, BigDecimal, BigDecimal)>,
+    min: BigDecimal,
+    max: BigDecimal
+) -> Vec<(char, BigDecimal, BigDecimal)> {
+    let mut interval: Vec<(char, BigDecimal, BigDecimal)> = Vec::new();
+
+    for item in start_interval {
+        let new_min = min.clone() + (max.clone() - min.clone()) * item.1.clone();
+        let new_max = min.clone() + (max.clone() - min.clone()) * item.2.clone();
+        interval.push((item.0, new_min, new_max));
+    }
+
+    interval
+}
+
+fn get_char_in_interval(
+    value: BigDecimal,
+    start_interval: Vec<(char, BigDecimal, BigDecimal)>
+) -> (BigDecimal, BigDecimal, char) {
+    let mut min = BigDecimal::from(0);
+    let mut max = BigDecimal::from(0);
+    let mut ch: char = ' ';
+
+    for item in start_interval {
+        if item.1 <= value && value <= item.2 {
+            min = item.1;
+            max = item.2;
+            ch = item.0;
+        }
+    }
+
+    return (min, max, ch);
+}
+
+fn encode_message(
+    message: String,
+    start_interval: Vec<(char, BigDecimal, BigDecimal)>
+) -> BigDecimal {
+    let mut step_interval = start_interval.clone();
+
+    for i in 0..message.chars().count() - 1 {
+        let item = step_interval
+            .iter()
+            .find(|item| item.0 == message.chars().nth(i).unwrap())
+            .unwrap();
+        let min = item.1.clone();
+        let max = item.2.clone();
+        step_interval = calculate_interval(start_interval.clone(), min, max);
+    }
+
+    let result = step_interval
+        .iter()
+        .find(|item| item.0 == message.chars().last().unwrap())
+        .unwrap();
+    result.1.clone()
+}
+
+fn decode_message(
+    encoded_message: BigDecimal,
+    start_interval: Vec<(char, BigDecimal, BigDecimal)>,
+    message_length: usize
+) -> String {
+    let mut decoded_message = String::new();
+    let mut step_value = encoded_message.clone();
+
+    for _ in 0..message_length {
+        let (min, high_boundary, ch) = get_char_in_interval(
+            step_value.clone(),
+            start_interval.clone()
+        );
+        step_value = (step_value.clone() - min.clone()) / (high_boundary.clone() - min.clone());
+        decoded_message.push(ch);
+    }
+
+    decoded_message
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let message: &str = "testingingedsffdss9jdslfjsldjslsf";
+    // let message: &str = "testingingedsffdss9jdslfjsldjslsffdfddfffdssfddsfd";
+    let message: &str = "малосимпатичныйдостопримечательность";
+
+    let alphabet = compute_freq_map(message.to_string());
+    let start_interval = start_interval(alphabet.clone());
+
+    let start = Instant::now();
+
+    let encoded_duration = Instant::now() - start;
+    let encoded_message = encode_message(message.to_string(), start_interval.clone());
+    let start = Instant::now();
+    let decoded_message = decode_message(
+        encoded_message.clone(),
+        start_interval.clone(),
+        message.chars().count()
+    );
+    let decoded_duration = Instant::now() - start;
+
+    println!("Encoded message - {}", encoded_message);
+    println!("Decoded message - {}", decoded_message);
+    println!("Encoded message time - {:?}", encoded_duration);
+    println!("Decoded message time - {:?}", decoded_duration);
 }
--- lab9/src/main.rs
+++ lab9/src/main.rs
@@ -5,7 +5,7 @@ fn lz77_compress(input: String) -> Vec<(usize, usize, char)> {
     let mut pos = 0;
 
     while pos < input.len() {
-        println!("pos: {}", pos);
+        // println!("pos: {}", pos);
 
         let buff_start = if pos >= buffer_size { pos - buffer_size } else { 0 };
 
@@ -16,22 +16,22 @@ fn lz77_compress(input: String) -> Vec<(usize, usize, char)> {
 
         let data: String = substring(&input, pos, buffer_size);
 
-        println!("input: {:?}", data);
+        // println!("input: {:?}", data);
 
         let buffer = substring(&input, buff_start, buffer_len);
 
-        println!("buffer: {:?}", buffer);
+        // println!("buffer: {:?}", buffer);
 
         let (length, offset) = find_biggest_substring(&buffer, &data);
 
-        println!("length: {}, offset: {}", length, offset);
+        // println!("length: {}, offset: {}", length, offset);
 
-        let next_char = input
-            .chars()
-            .nth(pos + length)
-            .unwrap();
+        let next_char = match input.chars().nth(pos + length) {
+            Some(c) => c,
+            None => '\0',
+        };
 
-        println!("pos-offset: {}, next_char: {}", buffer.len() - offset, next_char);
+        // println!("pos-offset: {}, next_char: {}", buffer.len() - offset, next_char);
 
         result.push((if offset == 0 { 0 } else { buffer.len() - offset }, length, next_char));
         pos += if length == 0 { 1 } else { length + 1 };
@@ -42,7 +42,8 @@ fn lz77_compress(input: String) -> Vec<(usize, usize, char)> {
     result
 }
 
-fn lz77_decompress(input: Vec<(usize, usize, char)>) -> String {
+fn lz77_decompress(input: &Vec<(usize, usize, char)>) -> String {
+    let input = input.clone();
     let mut result = String::new();
 
     for (offset, length, next_char) in input {
@@ -59,14 +60,36 @@ fn lz77_decompress(input: Vec<(usize, usize, char)>) -> String {
 
 fn main() {
     let input =
-        "u4FLi3szRR3tfNGGauJd3Jt1oJfRdGtHNEV6n90Fjk4J7CjIIQXqP9TTxzIk6rS5ArrM6wdXwr3AVfV47kog93nr3ojkUDNUOeZr!";
-    // println!("{:?}", input);
+        "DjQzRG9xvyQNSzwHd9EWeTlZQL7up8WDlXI1FmRLTUi4eUp3TYyRNvAcHPLQvL16YpsukA9jBKKmEVZFgmoJTG2
+        32M6zXwdWJsNoF85ZSTW3e24eLLS3VZZDKoR75VZQc3m56eMew5yT6KEwrHJv1r1HrIcV6QnOkQNYlja0nkM2NLQ
+        1Z6m462NrXNkjlxE3SKAepC89GFV8d7sYsOGOZ8eW8LdX1WHIxTneg9pU2amh29lOYJ6l7gPp6t5SjpFxD54hMJD
+        ulFgxxTFJIEQiYOhl2vrEeUO3LSfanhGRsXoPvYmt3IXllU9Sh9UUjSAVGtyMIRZC2LZ8le4pdw9ltf3pz1GaXyl
+        xWbJKMxJkmtqRpej2HtV4lwvUwzP7DyEDxTpLxUr1Pu3os7yYLNmLylxav0JfqYS7d3kbofWsiheVEFWhzeY9e5e
+        4aixszT6oXqlu0yY73Ucct8rlZrzpqTXK7iPaWDK7ooYyMLCneoODsazFpbea!";
+    // let input =
+    //     "banananababananafananaannaddaffdsfsdlkjojvjsl ksiojdf sdjfj sdklf; sfjiosfjos fjklsdfkljsdfs";
 
     let compressed = lz77_compress(input.to_string());
     println!("{:?}", compressed);
 
-    let decompressed = lz77_decompress(compressed);
-    println!("{}", decompressed);
+    let decompressed = lz77_decompress(&compressed);
+
+    println!("input \t{}", input);
+    println!("output \t{}", decompressed);
+
+    if input == decompressed.trim_end_matches('\0') {
+        println!("input and decompressed are equal");
+    } else {
+        println!("input and decompressed are not equal");
+    }
+
+    let input_len = (input.len() as f64) * 8.0;
+    let output_len = (compressed.len() as f64) * 8.0;
+    let r1 = output_len / input_len;
+    let r2 = 1.0 - r1;
+
+    println!("input_len: {}, output_len: {}", input_len, output_len);
+    println!("r1: {}, r2: {}", r1, r2);
 }
 
 /// Finds the biggest substring in the given string.
